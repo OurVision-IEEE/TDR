d88d088789372d1494476e60115004200950408c
tezz-io <matblockgreninja@gmail.com>
2021-08-02 23:32:01 +0530

    Added tensorflow boilercode and updated directories

--- build/mac/build.sh
+++ build/mac/build.sh
@@ -1,9 +1,13 @@
+export LIBRARY_PATH=$LIBRARY_PATH:~/Sources/tensorflow/lib
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:~/Sources/tensorflow/lib
 echo "-----------------------------------"
 echo "-- Running CMAKE"
 echo "-----------------------------------"
+cd build/mac
 cmake -S ../.. -B .
 echo "-----------------------------------"
 echo "-- Building CMAKE"
 echo "-----------------------------------"
 cmake --build .
 echo "-----------------------------------"
+cd ../..
