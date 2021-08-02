f3264e8146eb638859cea731d4e72f88b46bf4aa
tezz-io <matblockgreninja@gmail.com>
2021-08-02 23:32:28 +0530

    Generated Build files

---
+++ build/mac/CMakeFiles/tdr.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tdr.dir/src/Main.cc.o"
+  "CMakeFiles/tdr.dir/src/Main.cc.o.d"
+  "tdr"
+  "tdr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tdr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/mac/CMakeFiles/tdr.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for tdr.
+# This may be replaced when dependencies are built.
